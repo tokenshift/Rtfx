4d5f8cd60c56a3795556982badb288db703de641
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-08-16 15:44:44 -0400

    Stubbing out Writer functionality.

---
+++ Rtfx.Test/TestWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rtfx.Test {
+    [TestClass]
+    public class TestWriter {
+        [TestMethod]
+        public void TestWriteSpan() {
+            var buffer = new MemoryStream();
+            var writer = new Writer(buffer);
+
+            writer.Span("This is a test.");
+
+            var result = Encoding.UTF8.GetString(buffer.ToArray());
+            Assert.AreEqual("This is a test.", result);
+        }
+
+        [TestMethod]
+        public void TestWriteUnicodeSpan() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestWriteControlSymbolsInSpan()
+        {
+            var buffer = new MemoryStream();
+            var writer = new Writer(buffer);
+
+            writer.Span(@"This {is} a \ test.");
+
+            var result = Encoding.UTF8.GetString(buffer.ToArray());
+            Assert.AreEqual(@"This \{is\} a \\ test.", result);
+        }
+
+        [TestMethod]
+        public void TestWriteGroup() {
+            var buffer = new MemoryStream();
+            var writer = new Writer(buffer);
+
+            writer.GroupStart();
+            writer.Control("par");
+            writer.Span("This is a test.");
+            writer.GroupEnd();
+
+            var result = Encoding.UTF8.GetString(buffer.ToArray());
+            Assert.AreEqual(@"{\par This is a test.}", result);
+        }
+    }
+}
---
+++ Rtfx/Writer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Rtfx
+{
+    /// <summary>
+    /// Writes RTF data to an output stream.
+    /// </summary>
+    public class Writer :IDisposable {
+        private readonly Stream _output;
+
+        public void Dispose() {
+            _output.Dispose();
+        }
+
+        public Writer(Stream output) {
+            if (output == null) {
+                throw new ArgumentNullException("output");
+            }
+
+            _output = output;
+        }
+
+        #region RTF Output
+
+        /// <summary>
+        /// Writes a control word to the output stream.
+        /// </summary>
+        public void Control(string name, int? param = null) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes binary data to the output stream.
+        /// </summary>
+        public void Data(byte[] buffer) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a group end symbol ('}') to the output stream.
+        /// </summary>
+        public void GroupEnd() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a group start symbol ('{') to the output stream.
+        /// </summary>
+        public void GroupStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes plain text content to the output stream.
+        /// </summary>
+        public void Span(string content) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
